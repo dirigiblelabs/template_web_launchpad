b7f0aa1805e6ab26af40b46a1e40aee2711917b3
ThuF <jordan.p.pavlov@gmail.com>
2016-12-22 16:17:34 +0200

    Fixes generation of appTemplate.js

--- template_web_launchpad/WebContent/template_web_launchpad/ScriptingServices/templates/appTemplate.js
+++ template_web_launchpad/WebContent/template_web_launchpad/ScriptingServices/templates/appTemplate.js
@@ -1,10 +1,12 @@
+#set ( $dollar = "$")
+
 /*globals angular */
 var app = angular.module('launchpad', ['ngRoute', 'ng', 'defaultControllers']);
 
 var controllers = angular.module('defaultControllers', []);
 
-app.config(function($routeProvider) {
-	$routeProvider.when('/home', {
+app.config(function(${dollar}routeProvider) {
+	${dollar}routeProvider.when('/home', {
 		templateUrl: 'templates/home.html'
 	})
 	+++_ROUTES_PLACEHOLDER_+++
@@ -13,35 +15,35 @@ app.config(function($routeProvider) {
 	});
 });
 
-controllers.controller('LaunchpadCtrl', ['$scope', '$location', '$http',
-	function($scope, $location, $http) {
-		const API_HOME = '../api/home.js';
-		const API_MENU = '../api/menu.js';
+controllers.controller('LaunchpadCtrl', ['${dollar}scope', '${dollar}location', '${dollar}http',
+	function(${dollar}scope, ${dollar}location, ${dollar}http) {
+		const API_HOME = '../../js/${packageName}/api/home.js';
+		const API_MENU = '../../js/${packageName}/api/menu.js';
 
-		$scope.location = $location;
+		${dollar}scope.location = ${dollar}location;
 
-		$http.get(API_HOME).success(function (data) {
-			$scope.homeData = data;
-			loadPageFromPath($scope.homeData);
+		${dollar}http.get(API_HOME).success(function (data) {
+			${dollar}scope.homeData = data;
+			loadPageFromPath(${dollar}scope.homeData);
 		});
 
-		$http.get(API_MENU).success(function (data) {
-			$scope.menu = data;
-			loadPageFromPath($scope.menu);
+		${dollar}http.get(API_MENU).success(function (data) {
+			${dollar}scope.menu = data;
+			loadPageFromPath(${dollar}scope.menu);
 		});
 
-		$scope.setView = function(view) {
-			$scope.view = view;
+		${dollar}scope.setView = function(view) {
+			${dollar}scope.view = view;
 		};
 
-		$scope.setHomeView = function() {
-			$scope.setView('home.html');
+		${dollar}scope.setHomeView = function() {
+			${dollar}scope.setView('home.html');
 		};
 
 		function loadPageFromPath(data) {
 			for (var i = 0; i < data.length; i ++) {
-				if (data[i].path === $scope.location.path()) {
-					$scope.setView(data[i].link);
+				if (data[i].path === ${dollar}scope.location.path()) {
+					${dollar}scope.setView(data[i].link);
 				}
 			}
 		}
